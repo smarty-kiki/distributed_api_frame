b3ae99258dd74213abea501abafcd445f1e72d7a
yaoyang <kiki@smarty.so>
2018-03-03 23:44:25 +0800

    修改 bootstrap 加载 dep_domaindep_client

--- bootstrap.php
+++ bootstrap.php
@@ -5,10 +5,9 @@ date_default_timezone_set('Asia/Shanghai');
 
 define('ROOT_DIR', __DIR__);
 define('FRAME_DIR', ROOT_DIR.'/frame');
-define('DOMAIN_DIR', ROOT_DIR.'/domain');
-define('COMMAND_DIR', ROOT_DIR.'/command');
 define('CONTROLLER_DIR', ROOT_DIR.'/controller');
-define('QUEUE_JOB_DIR', COMMAND_DIR.'/queue_job');
+define('DEP_DOMAIN_DIR', ROOT_DIR.'/dep_domain');
+define('DEP_CLIENT_DIR', ROOT_DIR.'/dep_client');
 
 include FRAME_DIR.'/function.php';
 include FRAME_DIR.'/entity.php';
@@ -17,10 +16,10 @@ include FRAME_DIR.'/database/mysql.php';
 include FRAME_DIR.'/storage/mongodb.php';
 include FRAME_DIR.'/cache/redis.php';
 include FRAME_DIR.'/queue/beanstalk.php';
-include FRAME_DIR.'/unitofwork.php';
 
 config_dir(ROOT_DIR.'/config');
 
 include ROOT_DIR.'/util/load.php';
-include DOMAIN_DIR.'/load.php';
+include DEP_DOMAIN_DIR.'/load.php';
+include DEP_CLIENT_DIR.'/load.php';
 include QUEUE_JOB_DIR.'/load.php';
